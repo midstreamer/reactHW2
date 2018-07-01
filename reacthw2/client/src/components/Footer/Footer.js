3da995e9bf14684fbe021de520354492e3275acd
midstreamer <chadalessi@yahoo.com>
2018-07-01 15:07:58 -0500

    added styling to the home page

---
+++ reacthw2/client/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => (
+  <footer>
+    <hr />
+    <p className="pull-right">
+      <i className="fas fa-newspaper" aria-hidden="true" />
+    </p>
+  </footer>
+);
+
+export default Footer;
